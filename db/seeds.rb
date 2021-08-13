c831bea37f69341a70bd8c9406fdaeac2f02bb84
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-08-13 14:09:21 +0200

    mapbox works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Relai.destroy_all
+Florist.destroy_all
+
+puts "Creating the first Relais:"
+
+relai1 = Relai.new(
+  name: "Villa Augusta",
+  address: "14 Rue du Serre Blanc, Saint Paul 3 Châteaux",
+  longitude: "44.34531",
+  latitude: "4.766563",
+)
+relai1.save!
+
+puts "name : #{relai1.name}, au #{relai1.address}"
+
+puts 'creating a Master Florist:'
+
+florist1 = Florist.new(
+  name: "Fleurs de Provence",
+  address: "12 Rue du Serre Blanc, Saint Paul 3 Châteaux",
+  email: "sarl.vdb@wanadoo.fr",
+  phone: "0475973030",
+  description: "Magasin principale",
+  longitude: "44.34632",
+  latitude: "4.76651",
+)
+florist1.save!
+
+puts "name : #{florist1.name}, au #{florist1.address}"
+
+puts "Done."
