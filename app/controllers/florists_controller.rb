ebd34a8f956ff040da89d3b2ea40bfed35617d5f
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-08-14 14:17:03 +0200

    setting the pages and routes for all the pages and links

--- app/controllers/florists_controller.rb
+++ app/controllers/florists_controller.rb
@@ -1,9 +1,10 @@
 class FloristsController < ApplicationController
 
+  before_action :find_florist, only: [:composition, :evenement, :contact, :cgv]
+
 def home
   @florists = Florist.all
   @florist = Florist.find(params[:id])
-
     # the `geocoded` scope filters only florists with coordinates (latitude & longitude)
     @markers = @florists.geocoded.map do |f|
       {
@@ -13,7 +14,22 @@ def home
     end
 end
 
+  def composition
+  end
+
+  def evenement
+  end
+
+  def contact
+  end
+
+  def cgv
+  end
+
 private
+  def find_florist
+    @florist = Florist.find(params[:id])
+  end
 
   def florist_params
     params.require(:florist).permit(:name, :address, :latitude, :longitude)
