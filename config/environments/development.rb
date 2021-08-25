183b5d4dec918043a5b9cc2de48fea567c2f487c
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-08-25 18:35:39 +0200

    Cloudinary and image_tag

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Active storage for Cloudinary
+  config.active_storage.service = :cloudinary
+
   # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = false
 
