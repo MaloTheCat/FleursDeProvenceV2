d33869fa1f2dc03fe99bb92f5cc1d41e1ee5ddbd
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-08-25 13:50:56 +0200

    photo_title on compo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_073502) do
+ActiveRecord::Schema.define(version: 2021_08_25_085459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_073502) do
     t.bigint "florist_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo_title"
     t.index ["florist_id"], name: "index_compositions_on_florist_id"
   end
 
