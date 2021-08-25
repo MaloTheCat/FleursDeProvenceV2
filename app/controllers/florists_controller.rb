8bd6c5e9f4b71120ae0472a43135b381cec8a462
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-08-25 16:38:00 +0200

    id florist

--- app/controllers/florists_controller.rb
+++ app/controllers/florists_controller.rb
@@ -5,7 +5,7 @@ class FloristsController < ApplicationController
   def home
     @florists = Florist.all
     # @florist = Florist.find(params[:id])
-    @florist = Florist.find(id=6)
+    @florist = Florist.find(id=1)
 
     # the `geocoded` scope filters only florists with coordinates (latitude & longitude)
     @markers = @florists.geocoded.map do |f|
