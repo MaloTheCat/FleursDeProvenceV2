8c6f3c74fcbba7944d9ce8f17a99cd9958e5176d
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-08-25 15:58:03 +0200

    modifying images abnd restructured models

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,3 +1,17 @@
 class Composition < ApplicationRecord
   belongs_to :florist
+
+  def get_image_tag
+    image_tag = "#{photo_title}.png"
+    # image_tag = ""
+    # if photo_title.present?
+      # if photo_title.start_with?("https")
+      #   image_tag = photo_title
+      # else
+      # end
+    # end
+    # raise
+    # image_tag
+  end
+
 end
