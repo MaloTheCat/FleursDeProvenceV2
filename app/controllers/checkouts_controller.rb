b38a9ce097b9d1524ce4f95d2ec59d027d4ae2d2
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-10-21 23:17:07 +0200

    checkout controller for heroku

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,14 +1,14 @@
 class checkoutsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def show
-    current_user.processor = :stripe
-    current_user.customer
+    # current_user.processor = :stripe
+    # current_user.customer
 
-    @checkout_session = current_user.payment_processor.checkout(
-      mode: "payment";
-      line_items: "price_1JfLy7FE4oPSz5YOHCSrRNzo"
-    )
+    # @checkout_session = current_user.payment_processor.checkout(
+    #   mode: "payment";
+    #   line_items: "price_1JfLy7FE4oPSz5YOHCSrRNzo"
+    # )
   end
 
 end
