2530d8f1b9af6b5e32dfcb3a64c0dc68446bfbc9
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-11-11 12:00:56 +0100

    adding null

--- db/migrate/20210812124035_create_compositions.rb
+++ db/migrate/20210812124035_create_compositions.rb
@@ -1,10 +1,10 @@
 class CreateCompositions < ActiveRecord::Migration[6.1]
   def change
     create_table :compositions do |t|
-      t.string :name
+      t.string :name, null: false
 
-      t.integer :base_price
-      t.integer :quantity
+      t.integer :base_price, null: false, precision: 6, scale: 2
+      t.integer :quantity, null: false
       t.boolean :disponibility
       t.string :photo_title
       t.references :florist, null: false, foreign_key: true
