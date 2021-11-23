8ba87358c243da283ac1c3024625c95791f79b88
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-11-23 18:00:30 +0100

    Modifying root_url but still doesn't works

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -74,8 +74,8 @@ class OrdersController < ApplicationController
         currency: 'eur',
         quantity: '1'
       }],
-      success_url: order_url(@order),
-      cancel_url: order_url(@order)
+      success_url: root_url,
+      cancel_url: root_url
     )
 
     @order.update(checkout_session_id: @session.id)
