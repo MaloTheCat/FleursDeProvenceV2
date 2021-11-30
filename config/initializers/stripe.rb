2eb78c21566e929b9b9c2d428570acc37e755658
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-11-30 17:58:16 +0100

    a

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,14 @@
-Rails.configuration.stripe = {
-  # publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  # secret_key:      ENV['STRIPE_SECRET_KEY']
-  publishable_key: Rails.application.credentials[:stripe][:publishable_key]
-  secret_key:      Rails.application.credentials[:stripe][:secret_key]
+# Rails.configuration.stripe = {
+#   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+#   secret_key:      ENV['STRIPE_SECRET_KEY']
+#   # publishable_key: Rails.application.credentials[:stripe][:publishable_key]
+#   # secret_key:      Rails.application.credentials[:stripe][:secret_key]
 
-}
+# }
 
-Stripe.api_key = Rails.configuration.stripe[:stripe][:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+
+if Rails.application.credentials[:stripe].present? && Rails.application.credentials[:stripe][:secret_key].present?
+  Stripe.api_key = Rails.application.credentials[:stripe][:secret_key]
+end
