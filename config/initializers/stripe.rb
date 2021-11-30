263818cadafb65ed230d444b1a860f3dd72e4882
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-11-30 18:14:09 +0100

    removing credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,14 +1,15 @@
-# Rails.configuration.stripe = {
-#   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-#   secret_key:      ENV['STRIPE_SECRET_KEY']
-#   # publishable_key: Rails.application.credentials[:stripe][:publishable_key]
-#   # secret_key:      Rails.application.credentials[:stripe][:secret_key]
+Rails.configuration.stripe = {
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key:      ENV['STRIPE_SECRET_KEY']
+  signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
+  # publishable_key: Rails.application.credentials[:stripe][:publishable_key]
+  # secret_key:      Rails.application.credentials[:stripe][:secret_key]
 
-# }
+}
 
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
-
-if Rails.application.credentials[:stripe].present? && Rails.application.credentials[:stripe][:secret_key].present?
-  Stripe.api_key = Rails.application.credentials[:stripe][:secret_key]
-end
+# if Rails.application.credentials[:stripe].present? && Rails.application.credentials[:stripe][:secret_key].present?
+#   Stripe.api_key = Rails.application.credentials[:stripe][:secret_key]
+# end
