4b90ca23e750e6a9a3689155d0d3a073d7da476c
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-12-01 11:19:22 +0100

    a

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  # publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
   secret_key: ENV['STRIPE_SECRET_KEY']
   # signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
 }
