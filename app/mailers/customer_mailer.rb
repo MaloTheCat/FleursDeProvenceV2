28207be895444ed3581ccde6d27319b7682fbe16
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-12-17 13:13:59 +0100

    CustomerMailer

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,7 +1,6 @@
 class CustomerMailer < ApplicationMailer
   def customer_mail(contact)
     @order.customer.email = contact
-    # @order
     mail to: @order.customer.email, subject: "Résumé de votre commande Fleurs de Provence"
   end
 end
