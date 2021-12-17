d5f862a36974859d5610a6e5f9df97130ff635bc
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-12-17 16:00:42 +0100

    CustomerMailer

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,11 +1,7 @@
 class CustomerMailer < ApplicationMailer
-  def customer_mail(contact)
+  def customer_mail(contact, order)
+    @order = order
     @customer = contact
-    session = event.data.object
-    @order = Stripe::Checkout::Session.retrieve({
-        id: session.id,
-        expand: ["line_items", "customer"]
-      })
     mail to: @customer, subject: "Votre commande Fleurs de Provence"
   end
 end
