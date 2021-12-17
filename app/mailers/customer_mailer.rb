2ecac69bd660f6b376688cea8ae3ca96b069a69d
Jonathan Vanderbosse <jonathan.vanderbosse@gmail.com>
2021-12-17 15:35:57 +0100

    CustomerMailer

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,6 +1,7 @@
 class CustomerMailer < ApplicationMailer
   def customer_mail(contact)
     @customer = contact
+    session = event.data.object
     @order = Stripe::Checkout::Session.retrieve({
         id: session.id,
         expand: ["line_items", "customer"]
